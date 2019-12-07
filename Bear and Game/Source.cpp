edc2ad5ccb1f4558503ad73609f8d0538804f034
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2019-12-06 23:14:05 -0800

    Add files via upload

--- Bear and Game/Source.cpp
+++ Bear and Game/Source.cpp
@@ -7,47 +7,50 @@
 
 
 #include<iostream>
+#include<cmath>
+#include<cstdio>
 using namespace std;
 
 int main()
 {
-	int input;
-	cin >> input;
+    int input;
 
-	int minuteArray[91];
-	minuteArray[0] = 0;
-	for (int i = 1; i <= input; i++)
-		cin >> minuteArray[i];
+    cin >> input;
+    int minuteArray[input];
+    minuteArray[input] = 90;
 
-	for (int i = 0; i <= input; i++)
-	{
-		if (minuteArray[input] <= 15 /*&& minuteArray[1] <= 15*/)  // while the last element of the array is less 15, we only need to print the sum of last element & 15
-		{
-			cout << minuteArray[input] + 15 << endl;
-			break;
-		}
-		else if (minuteArray[input] >= 75 && input >= 6)  // while the last element of the array is bigger than 75 and total element in the array is bigger than or equal to 6, then we only need to print 90 
-		{
-			cout << 90 << endl;
-			break;
-		}
-		else if (minuteArray[i + 1] - minuteArray[i] <= 15 /*&& minuteArray[i + 1] < 75*/)
-		{
-			while (i == input)
-			{
-				cout << minuteArray[i] << endl;
-				break;
-			}
-			continue;
-		}
-		else /*if (minuteArray[i] < 75)*/
-		{
-			cout << minuteArray[i] + 15 << endl;
-			break;
-		}
-	}
+    for (int i = 0; i < input; i++)
+        cin >> minuteArray[i];
 
-	cout << endl;
-	system("pause");
-	return 0;
+    for (int i = 0; i < input; i++)
+    {
+        if (minuteArray[0] > 15)
+        {
+            cout << 15 << endl;
+            break;
+        }
+        else if (minuteArray[i + 1] - minuteArray[i] <= 15)
+        {
+            if (i + 1 != input)
+                continue;
+            else if (minuteArray[input] >= 75)
+            {
+                cout << 90 << endl;
+                break;
+            }
+            else
+            {
+                cout << minuteArray[i] + 15 << endl;
+                break;
+            }
+        }
+        else
+        {
+            cout << minuteArray[i] + 15 << endl;
+            break;
+        }
+    }
+
+    cout << endl;
+    return 0;
 }
