5810562dbb35b7f0aa83326954fcd0f675b0a4da
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2019-12-19 12:24:47 -0800

    Add files via upload

--- Vladik and Courtesy/Source.cpp
+++ Vladik and Courtesy/Source.cpp
@@ -1,1 +1,40 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Chowdhury
+// Codeforces - Vladik and Courtesy
+// Problem Statement - http://codeforces.com/contest/811/problem/A
+// Main.cpp
 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	long long int a, b, i;
+	cin >> a >> b;
+	int j = 1;
+	i = 1;
+
+	while (a >= 0 && b >= 0)
+	{
+		if (j == 1)
+		{
+			a = a - i;
+			j = 0;
+			i++;
+		}
+		else if (j == 0)
+		{
+			b = b - i;
+			j = 1;
+			i++;
+		}
+	}
+
+	if (a < 0)
+		cout << "Vladik" << endl;
+	else
+		cout << "Valera" << endl;
+
+	return 0;
+}
