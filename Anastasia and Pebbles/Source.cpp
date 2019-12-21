53a7e47641020d5cd2dff42959492e88c943fa2b
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2019-12-20 20:00:46 -0800

    Add files via upload

--- Anastasia and Pebbles/Source.cpp
+++ Anastasia and Pebbles/Source.cpp
@@ -1,1 +1,32 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Chowdhury
+// Codeforces - 407A - Anastasia and Pebbles
+// Problem Statement - http://codeforces.com/contest/789/problem/A?mobile=false
+// Main.cpp
 
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+	int n, w;
+	long int answer;
+	double day = 0;
+	long double k;
+
+	cin >> n >> k;
+	int arr[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+		day = day + ceil(arr[i] / k);
+	}
+	answer = ceil(day / 2);
+
+	cout << answer << endl;
+
+	return 0;
+}
