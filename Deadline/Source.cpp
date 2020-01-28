ac61e9462e36422234b458f1690f51c17f44f96c
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2020-01-27 19:13:42 -0800

    Add files via upload

--- Deadline/Source.cpp
+++ Deadline/Source.cpp
@@ -1,1 +1,32 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Chowdhury
+// Codeforces - Deadline
+// Problem Statement - http://codeforces.com/contest/1288/problem/A
+// Main.cpp
 
+
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+	int n, d, testCases, answer, temp;
+
+	cin >> testCases;
+	for (int i = 0; i < testCases; i++)
+	{
+		cin >> n >> d;
+
+		temp = sqrt(d);
+		answer = (temp - 1) + ((d + temp - 1) / temp);
+
+		if (answer <= n)
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
+	}
+
+	return 0;
+}
